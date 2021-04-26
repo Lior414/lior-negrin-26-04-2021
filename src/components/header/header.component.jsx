226129df99494c863376a8fc86c96082deac9a6b
Lior414 <ngrin.lior@gmail.com>
2021-04-27 00:27:06 +0300

    added bootstrap css to index,js

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,21 +1,21 @@
 import React from 'react';
 import { Navbar, Nav, Container } from 'react-bootstrap';
 
-
 function Header(){
     return(
         <div>
             <Container>
-                <Navbar bg="light" expand="lg">
-                <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
-                <Navbar.Toggle aria-controls="basic-navbar-nav" />
-                <Navbar.Collapse id="basic-navbar-nav">
-                <Nav className="mr-auto">
-                <Nav.Link href="#home">Home</Nav.Link>
-                <Nav.Link href="#link">Link</Nav.Link>
-                </Nav>
-                </Navbar.Collapse>
-                </Navbar>
+            <Navbar bg="dark" variant="dark" expand="lg" sticky="top">
+                                <Navbar.Brand href="#home">React Bootstrap Navbar</Navbar.Brand>
+                                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                                <Navbar.Collapse id="basic-navbar-nav">
+                                    <Nav className="mr-auto">
+                                    <Nav.Link href="/">Home</Nav.Link>
+                                    <Nav.Link href="/about-us">Contact Us</Nav.Link>
+                                    <Nav.Link href="/contact-us">About Us</Nav.Link>
+                                    </Nav>
+                                </Navbar.Collapse>
+                            </Navbar>
             </Container>
         </div>
     )   
